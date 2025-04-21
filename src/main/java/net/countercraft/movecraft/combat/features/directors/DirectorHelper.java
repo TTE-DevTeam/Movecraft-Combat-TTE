9640724beb5e8de6d1ce8ebff48316ea2c10d6f5
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-04-21 23:57:21 +0200

    add methods for flagging tnt

--- src/main/java/net/countercraft/movecraft/combat/features/directors/DirectorHelper.java
+++ src/main/java/net/countercraft/movecraft/combat/features/directors/DirectorHelper.java
@@ -1,16 +1,40 @@
 package net.countercraft.movecraft.combat.features.directors;
 
 import net.countercraft.movecraft.combat.MovecraftCombat;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.SinkingCraft;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.persistence.PersistentDataType;
 
 public class DirectorHelper {
 
-    static final NamespacedKey DIRECTOR_HINT = new NamespacedKey(MovecraftCombat.getInstance(), "director_node");
+    static final NamespacedKey KEY_DIRECTOR_NODE = new NamespacedKey(MovecraftCombat.getInstance(), "director_node");
+    static final NamespacedKey KEY_FLAGGED_FOR_DIRECTION = new NamespacedKey(MovecraftCombat.getInstance(), "director_needs_direction");
 
     public static void flagEntity(Entity entity, String value) {
-        entity.getPersistentDataContainer().set(DIRECTOR_HINT, PersistentDataType.STRING, value);
+        entity.getPersistentDataContainer().set(KEY_DIRECTOR_NODE, PersistentDataType.STRING, value);
+    }
+
+    public static boolean isTNTFlaggedForDirection(TNTPrimed tntPrimed) {
+        return tntPrimed.getPersistentDataContainer().getOrDefault(KEY_FLAGGED_FOR_DIRECTION, PersistentDataType.BOOLEAN, false);
+    }
+
+    public static void flagTNTForDirection(TNTPrimed tntPrimed, final Craft craft) {
+        if (craft == null) {
+            return;
+        }
+        if (craft instanceof SinkingCraft) {
+            return;
+        }
+        if (!craft.getType().getBoolProperty(CannonDirectors.ALLOW_CANNON_DIRECTOR_SIGN)) {
+            return;
+        }
+        if (isTNTFlaggedForDirection(tntPrimed)) {
+            return;
+        }
+        tntPrimed.getPersistentDataContainer().set(KEY_FLAGGED_FOR_DIRECTION, PersistentDataType.BOOLEAN, true);
     }
 
     public static String getDirectorNameFrom(Entity entity) {
@@ -18,8 +42,8 @@ public class DirectorHelper {
             return null;
         }
 
-        if (entity.getPersistentDataContainer().has(DIRECTOR_HINT, PersistentDataType.STRING)) {
-            return entity.getPersistentDataContainer().get(DIRECTOR_HINT, PersistentDataType.STRING);
+        if (entity.getPersistentDataContainer().has(KEY_DIRECTOR_NODE, PersistentDataType.STRING)) {
+            return entity.getPersistentDataContainer().get(KEY_DIRECTOR_NODE, PersistentDataType.STRING);
         }
         return null;
     }
