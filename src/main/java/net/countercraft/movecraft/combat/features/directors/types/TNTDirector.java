0d5b3c75f44a2f66887e1cae9b44e2699e0b99e8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-07 14:46:55 +0200

    finish TNTDirector

--- src/main/java/net/countercraft/movecraft/combat/features/directors/types/TNTDirector.java
+++ src/main/java/net/countercraft/movecraft/combat/features/directors/types/TNTDirector.java
@@ -8,10 +8,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Map;
 import java.util.function.Function;
 
-public class TNTDirector extends HorizontalSingleUserDirector {
+public class TNTDirector extends AbstractHorizontalSingleUserDirector {
 
     public TNTDirector(Map<String, Object> rawData) {
         super(rawData);
+    }
+
+    @Override
+    protected void deserialize(Map<String, Object> rawData) {
+        // we dont have custom data, ignore it!
     }
 
     @Override
@@ -21,11 +26,11 @@ public class TNTDirector extends HorizontalSingleUserDirector {
 
     @Override
     protected Triple<String, NamespacedKey, Function<CraftType, Double>> getMaxAngleCraftTypeDoubleProperty() {
-        return null;
+        return Triple.of("maxTNTDirectorAngle", new NamespacedKey("movecraft-combat", "max_tnt_director_angle"), c -> 60.0D);
     }
 
     @Override
     protected Triple<String, NamespacedKey, Function<CraftType, Boolean>> getAllowedOnCraftCraftTypeBooleanProperty() {
-        return null;
+        return Triple.of("allowTNTDirector", new NamespacedKey("movecraft-combat", "allow_tnt_director"), c -> false);
     }
 }
